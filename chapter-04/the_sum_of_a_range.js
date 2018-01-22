71918961ff5b491b0cacf2ebe6d8cb96d1e681b0
Sergio Peña <sergiop79@gmail.com>
2018-01-22 18:06:49 +0100

    Added step argument to range function

--- chapter-04/the_sum_of_a_range.js
+++ chapter-04/the_sum_of_a_range.js
@@ -1,7 +1,15 @@
-function range (start, finish) {
+function range (start, finish, step) {
   var output = []
-  for (var i = start; i <= finish; i++) {
-    output.push(i)
+  step = step || 1
+
+  if (step < 0) {
+    for (var i = start; i >= finish; i += step) {
+      output.push(i)
+    }
+  } else {
+    for (var j = start; j <= finish; j += step) {
+      output.push(j)
+    }
   }
 
   return output
@@ -18,3 +26,4 @@ function sum (array) {
 
 console.log(range(1, 10)) // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 console.log(sum(range(1, 10))) // 55
+console.log(range(5, 2, -1)) // → [5, 4, 3, 2]
