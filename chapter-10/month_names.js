4ae615345c945fc319b95eed7d5a40bd2f8d4ffe
Sergio Peña <sergiop79@gmail.com>
2018-02-08 17:35:16 +0100

    Added month names exercise

---
+++ chapter-10/month_names.js
@@ -0,0 +1,30 @@
+var month = (function () {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+  return {
+    name: function (num) {
+      return months[num]
+    },
+    number: function (name) {
+      return months.indexOf(name)
+    }
+  }
+}())
+
+console.log(month.name(2))
+// → March
+console.log(month.number('November'))
+// → 10
